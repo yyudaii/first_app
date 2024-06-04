22fbbe855ca0c2c0f1d1cf26c2d817a4f46c2a24
yyudaii <yudai200009089311@gmail.com>
2024-06-04 17:21:55 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"]
+  get 'posts' , to:'posts#index'
+  get 'posts/new', to: 'posts#new'
+  post 'posts', to: 'posts#create'
+end
